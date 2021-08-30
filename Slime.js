5f6560be6c73994e622d1083d8c78dc9ea1050c7
Vibhor Singh <78255056+Vibhor007-dev@users.noreply.github.com>
2021-08-30 12:26:27 +0530

    Add files via upload

---
+++ Slime.js
@@ -0,0 +1,28 @@
+class Slime{
+    constructor(x,y)
+	{
+		var options={
+		'restitution':3,
+		'friction':1,
+		'density':8,
+		}
+		this.body=Bodies.rectangle(x,y,50,30,options);
+		this.width=50;
+		this.height=50;
+		World.add(world,this.body);
+	}
+	display(){
+		var pos=this.body.position;
+		var angle=this.body.angle;
+		push();
+		translate(pos.x,pos.y);
+		rotate(angle);
+		fill("green");
+		strokeWeight(4);
+		stroke("black");
+
+		rect(0,0,this.width,this.height);
+		pop();
+	}
+
+}
